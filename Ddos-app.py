56a27e2a2ca8d5644fb926cdaea5df21781b64d2
Idriz Mirena <115082620+IdrizMirena@users.noreply.github.com>
2025-04-17 09:56:20 +0200

    Update Ddos-app.py

--- Ddos-app.py
+++ Ddos-app.py
@@ -13,7 +13,7 @@ for i in range(2000):
     file_name = f"file_{i+1}.txt"
     file_path = os.path.join(desktop, file_name)
     with open(file_path, "w") as f:
-        f.write(f"Kjo është skedari numër {i+1}.\n")
+        f.write(f"file number: {i+1}.\n")
 
 # Hap 1000  file prej tyne dhe i hap qata
 for i in range(1000):
